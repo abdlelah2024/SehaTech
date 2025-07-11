d4bcec58acdbd089e8b1a0888804c0459c3110f6
asd asd <asd19082@gmail.com>
2025-07-11 23:24:56 +0000

    Next

--- src/components/dashboard/appointments-tab.tsx
+++ src/components/dashboard/appointments-tab.tsx
@@ -43,7 +43,7 @@ export function AppointmentsTab({ searchTerm }: AppointmentsTabProps) {
 
 
   return (
-    <Card className="mt-4">
+    <Card>
        <CardHeader className="flex flex-row items-center justify-between">
           <div>
             <CardTitle>Appointments</CardTitle>
