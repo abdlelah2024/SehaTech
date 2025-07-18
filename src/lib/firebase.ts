cb2a69e318f19e8779e2497bb5d5ca91d50bf058
asd asd <asd19082@gmail.com>
2025-07-18 21:20:13 +0000

    // For Firebase JS SDK v7.20.0 and later, measurementId is optional cons

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,14 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  apiKey: "AIzaSyC2oCiZp9TTeF30GvaYu3fwpV0iLf8PfLY",
+  authDomain: "peppy-strategy-685.firebaseapp.com",
+  projectId: "peppy-strategy-685",
+  storageBucket: "peppy-strategy-685.appspot.com",
+  messagingSenderId: "978780666051",
+  appId: "1:978780666051:web:0e547933d89f8bc0570a76",
+  measurementId: "G-WY5B4QWGHL",
+  databaseURL: "https://peppy-strategy-685-default-rtdb.firebaseio.com",
 };
 
 // Initialize Firebase
